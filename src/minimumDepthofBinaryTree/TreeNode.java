fa2d7bff0c945fec264ec7873a63052babdb1342
Eric Jin <ericjin0819@gmail.com>
2018-11-19 10:26:38 +0800

    add minimum depth binary tree

---
+++ src/minimumDepthofBinaryTree/TreeNode.java
@@ -0,0 +1,16 @@
+package minimumDepthofBinaryTree;
+
+/**
+ * Create by IntelliJ IDEA.
+ * Author: EricJin
+ * Date: 11/14/2018 10:06 AM
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
