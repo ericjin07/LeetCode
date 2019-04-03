e0888c70c3dca29a578520df81cd51f9ca0ad0d4
Eric Jin <ericjin0819@gmail.com>
2019-04-03 22:15:28 +0800

    leetcode

---
+++ src/array/maximumAverageSubarray/MaximumAverageSubarrayTest.java
@@ -0,0 +1,20 @@
+package array.maximumAverageSubarray;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Create by IntelliJ IDEA.
+ * Author: EricJin
+ * Date: 04/03/2019 9:55 PM
+ */
+public class MaximumAverageSubarrayTest {
+
+    @Test
+    public void findMaxAverage() {
+        int[] nums = new int[]{0,4,0,3,2};
+        MaximumAverageSubarray solution = new MaximumAverageSubarray();
+        System.out.println(solution.findMaxAverage(nums,1));
+    }
+}
