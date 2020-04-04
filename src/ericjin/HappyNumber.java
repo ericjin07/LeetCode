de40fede89063143307d7f757e42b220122b341d
Eric Jin <ericjin0819@gmail.com>
2020-04-04 09:56:36 +0800

    Is a Happy number

---
+++ src/ericjin/HappyNumber.java
@@ -0,0 +1,9 @@
+package ericjin;
+
+/**
+ * Create by IntelliJ IDEA.
+ * Author: Eric Jin
+ * Date: 4/3/20 11:19 PM
+ */
+public class HappyNumber {
+}
---
+++ src/ericjin/HappyNumberTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+/**
+ * Create by IntelliJ IDEA.
+ * Author: Eric Jin
+ * Date: 4/3/20 11:36 PM
+ */
+public class HappyNumberTest {
+
+}
