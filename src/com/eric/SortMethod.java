6712a6606fc58bca4f5db30f5885a442cdb0e814
Eric Jin <ericjin0819@gmail.com>
2020-05-28 21:06:18 +0800

    Add merge sort and quick sort

--- src/com/eric/SortMethod.java
+++ src/com/eric/SortMethod.java
@@ -11,10 +11,12 @@ public class SortMethod {
 
     public static void main(String[] args) {
         SortMethod method = new SortMethod();
-        int[] arr = new int[] {3,2,1,5,4,6,7,9,8,0};
+        int[] arr = new int[] {3,2,1,5,4,6,7,9,8,0,12,10,11,88,67,34,54,77,56,32,100};
 //        System.out.println(Arrays.toString(method.bubbleSort(arr)));
 //        System.out.println(Arrays.toString(method.insertSort(arr)));
-        System.out.println(Arrays.toString(method.selectSort(arr)));
+//        System.out.println(Arrays.toString(method.selectSort(arr)));
+//        System.out.println(Arrays.toString(method.mergeSort(arr)));
+        System.out.println(Arrays.toString(method.quickSort(arr)));
     }
 
     public int[] bubbleSort(int[] arr) {
@@ -68,6 +70,70 @@ public class SortMethod {
             }
         }
         return arr;
+    }
+
+    // 归并排序
+    public int[] mergeSort(int[] arr) {
+        mergeSortHelper(arr, 0, arr.length - 1);
+        return arr;
+    }
+
+    private void mergeSortHelper(int[] arr, int lo, int hi) {
+        // end point
+        if (lo >= hi) return;
+        int mid = (lo + hi) / 2;
+        // break into small problem
+        mergeSortHelper(arr, lo, mid);
+        mergeSortHelper(arr, mid + 1, hi);
+        // merge the two sorted into origin
+        int[] tmp = new int[hi - lo + 1];
+        int p1 = lo, p2 = mid + 1, i = 0;
+        while (p1 <= mid && p2 <= hi) {
+            if (arr[p1] <= arr[p2]) {
+                tmp[i++] = arr[p1++];
+            } else {
+                tmp[i++] = arr[p2++];
+            }
+        }
+        for (; p1 <= mid && i < tmp.length; i++) {
+            tmp[i] = arr[p1++];
+        }
+        for (; p2 <= hi && i < tmp.length; i++) {
+            tmp[i] = arr[p2++];
+        }
+        for (int j = lo, k = 0; j <= hi; j++, k++) {
+            arr[j] = tmp[k];
+        }
+    }
+
+    //快速排序
+    public int[] quickSort(int[] arr) {
+        quickSortHelper(arr, 0, arr.length - 1);
+        return arr;
+    }
+
+    private void quickSortHelper(int[] arr, int lo, int hi) {
+        if (lo >= hi) return;
+        int pivot = partition(arr, lo, hi);
+
+        quickSortHelper(arr, lo, pivot - 1);
+        quickSortHelper(arr, pivot + 1, hi);
+    }
+
+    private int partition(int[] arr, int lo, int hi) {
+        int pivot = arr[hi];
+        int i = lo;
+        for (int j = lo; j < hi; j++) {
+            if (arr[j] < pivot) {
+                int tmp = arr[j];
+                arr[j] = arr[i];
+                arr[i++] = tmp;
+            }
+        }
+        int tmp = arr[i];
+        arr[i] = pivot;
+        arr[hi] = tmp;
+        return i;
     }
 
 }
