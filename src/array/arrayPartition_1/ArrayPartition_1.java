e49164d843299a366fad39b7d863da54df7e83e8
Eric Jin <ericjin0819@gmail.com>
2021-02-16 21:15:58 +0800

    Add Trim Binary Search Tree

--- src/array/arrayPartition_1/ArrayPartition_1.java
+++ src/array/arrayPartition_1/ArrayPartition_1.java
@@ -5,7 +5,6 @@ package array.arrayPartition_1;
  * Author: EricJin
  * Date: 03/07/2019 9:23 AM
  */
-
 import java.util.Arrays;
 
 /**
@@ -32,5 +31,21 @@ public class ArrayPartition_1 {
             sum += Math.min(nums[i],nums[i+1]);
         }
         return sum;
+    }
+
+    /**
+     * 大的数字会被遗弃，所以我们能保留的就是第二大的
+     * 因此我们对数组进行排序，让相邻的，总是第二大的那个数可以留下来
+     * @param nums arr
+     * @return the number of max sum
+     */
+    public int arrayPairSum_2(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        int res = 0;
+        for (int i = 0; i < len; i+=2) {
+            res += nums[i];
+        }
+        return res;
     }
 }
