18853c94f28e76941129209326cf6bc281199bfa
Eric Jin <ericjin0819@gmail.com>
2021-03-03 15:12:54 +0800

    Add a solution to Merge Sorted Array

--- src/array/mergeSortedArray/MergeSortedArray.java
+++ src/array/mergeSortedArray/MergeSortedArray.java
@@ -75,4 +75,16 @@ public class MergeSortedArray {
             nums1[k--] = nums2[j--];
         }
     }
+
+    // two point
+    public void merge_2(int[] nums1, int m, int[] nums2, int n) {
+        int i, j, k;
+        for (i = m - 1, j = n - 1, k = m + n -1; i >= 0 && j >= 0 && k >= 0;k--) {
+            if (nums1[i] > nums2[j]) nums1[k] = nums1[i--];
+            else nums1[k] = nums2[j--];
+        }
+        for (;j >= 0 && k >= 0; k--) {
+            nums1[k] = nums2[j--];
+        }
+    }
 }
