7dc1b8eafca121a91cbf92f9862ad7a2287f118b
Eric Jin <ericjin0819@gmail.com>
2021-03-07 20:52:38 +0800

    Add Minimum Window Substring

--- README.md
+++ README.md
@@ -29,6 +29,7 @@
 |69| [Sqrt(x)](https://leetcode.com/problems/sqrtx/description/) | [Java](src/math/sqrtofx/SqrtOfX.java) |Easy|
 |70| [Climbing Stairs](https://leetcode.com/problems/climbing-stairs/description/) | [Java](src/dynamicProgram/climbingStairs/ClimbingStairs.java) |Easy|
 |75| [Sort Colors](https://leetcode.com/problems/sort-colors/) | [Java](src/array/sortColors/SortColors.java) |Easy|
+|76| [Minimum Window Substring](https://leetcode-cn.com/problems/minimum-window-substring/) | [Java](src/string/minimumWindowSubstring/MinimumWindowSubstring.java) |Hard|
 |83| [Remove Duplicates from Sorted List](https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/) | [Java](src/linkedList/removeDuplicatesfromSortedList/RemoveDuplicatesfromSortedList.java) |Easy|
 |88| [Merge Sorted Array](https://leetcode.com/problems/merge-sorted-array/description/) | [Java](src/array/mergeSortedArray/MergeSortedArray.java) |Easy|
 |100| [Same Tree](https://leetcode.com/problems/same-tree/description/) | [Java](src/tree/sameTree/SameTree.java) |Easy|
--- src/string/minimumWindowSubstring/MinimumWindowSubstring.java
+++ src/string/minimumWindowSubstring/MinimumWindowSubstring.java
@@ -1,13 +1,37 @@
 package string.minimumWindowSubstring;
 
-import java.util.HashMap;
-import java.util.HashSet;
-
 public class MinimumWindowSubstring {
 
     public String minWindow(String s, String t) {
-        HashMap<Character, Integer> map = new HashMap<>();
-//        map.
-        return "";
+        boolean[] exists = new boolean[128];
+        int[] chars = new int[128];
+        int tLen = t.length();
+        for (int i = 0; i < tLen; i++) {
+            chars[t.charAt(i)]++;
+            exists[t.charAt(i)] = true;
+        }
+
+        int sLen = s.length();
+        int cnt = 0;
+        int min = sLen + 1, minL = 0;
+        for (int r = 0, l = 0; r < sLen; r++) {
+            char c = s.charAt(r);
+            if (exists[c]) {
+                if (--chars[c] >= 0) {
+                    cnt++;
+                }
+                while (cnt == tLen) {
+                    if (r - l + 1  < min) {
+                        min = r - l + 1;
+                        minL = l;
+                    }
+                    if (exists[s.charAt(l)] && ++chars[s.charAt(l)] > 0) {
+                        cnt--;
+                    }
+                    l++;
+                }
+            }
+        }
+        return min > sLen ? "" : s.substring(minL, minL + min);
     }
 }
