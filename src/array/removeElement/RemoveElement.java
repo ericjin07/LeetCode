9b0d1251dcfc4f4cbefbbf16d4075b6dfcdbc108
Eric Jin <ericjin0819@gmail.com>
2021-04-19 21:23:56 +0800

    Modify remove element

--- src/array/removeElement/RemoveElement.java
+++ src/array/removeElement/RemoveElement.java
@@ -53,10 +53,7 @@ public class RemoveElement {
         int index = 0;
         for (int i = 0; i< nums.length;i++){
             if (nums[i] != val) {
-                nums[index] = nums[i];
-                index++;
-            } else {
-                continue;
+                nums[index++] = nums[i];
             }
         }
         return index;
