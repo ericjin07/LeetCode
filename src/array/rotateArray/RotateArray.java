514caee9494fb58f97f2d1bd279994f8caa5d64c
Eric Jin <ericjin0819@gmail.com>
2022-01-29 19:45:57 +0800

    2021-1-29 Add a solution to solve the Rotate Array

--- src/array/rotateArray/RotateArray.java
+++ src/array/rotateArray/RotateArray.java
@@ -49,4 +49,26 @@ public class RotateArray {
             nums[i] = a[i];
         }
     }
+
+    /**
+     * use reverse to rotate the shift
+     * @param nums
+     * @param k
+     */
+    public void rotate_reverse(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k , nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+    }
 }
