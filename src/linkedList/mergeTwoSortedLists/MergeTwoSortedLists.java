98c238c1730fe482029b157a19af0bff0cba8208
Eric Jin <ericjin0819@gmail.com>
2022-02-06 22:34:16 +0800

    2021-2-6 Add a solution for merge two sorted lists

--- src/linkedList/mergeTwoSortedLists/MergeTwoSortedLists.java
+++ src/linkedList/mergeTwoSortedLists/MergeTwoSortedLists.java
@@ -33,5 +33,21 @@ public class MergeTwoSortedLists {
         curr.next = l1 == null ? l2 : l1;
         return first.next;
     }
+
+    public ListNode mergeTwoLists_recursion(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1.val > list2.val) {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        } else {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }
+    }
 }
 
