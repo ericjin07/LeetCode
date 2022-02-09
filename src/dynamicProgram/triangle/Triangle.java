965931e548f6878bebdda73462267a3105b99d94
Eric Jin <ericjin0819@gmail.com>
2022-02-09 21:27:41 +0800

    Add solution for triangle

--- src/dynamicProgram/triangle/Triangle.java
+++ src/dynamicProgram/triangle/Triangle.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class Triangle {
 
+    /**
+     * from bottom to top  --better
+     */
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         if (n == 1) {
@@ -17,6 +20,28 @@ public class Triangle {
             }
         }
         return dp[0];
+    }
+
+    /**
+     * from top to bottom
+     */
+    public int minimumTotal_2(List<List<Integer>> triangle) {
+        if (triangle.size() == 1) {
+            return triangle.get(0).get(0);
+        }
+        int[] dp = new int[triangle.size()];
+        for (List<Integer> row : triangle) {
+            for (int i = row.size() - 1; i >= 0; i--) {
+                if (i == 0) dp[i] = dp[i] + row.get(i);
+                else if (i == row.size() - 1) dp[i] = dp[i - 1] + row.get(i);
+                else dp[i] = Math.min(dp[i], dp[i - 1]) + row.get(i);
+            }
+        }
+        int res = Integer.MAX_VALUE;
+        for (int i = 0; i < dp.length; i++) {
+            res = Math.min(res, dp[i]);
+        }
+        return res;
     }
 
 }
