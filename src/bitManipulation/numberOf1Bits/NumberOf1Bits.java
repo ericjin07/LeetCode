4b1d640bf6cf4d7ed92f79cd24f6b559e1ab7794
Eric Jin <ericjin0819@gmail.com>
2022-02-09 11:43:44 +0800

    Add a solution to Number Of 1 bits

--- src/bitManipulation/numberOf1Bits/NumberOf1Bits.java
+++ src/bitManipulation/numberOf1Bits/NumberOf1Bits.java
@@ -6,7 +6,19 @@ public class NumberOf1Bits {
         int cnt = 0;
         while (n != 0) {
             cnt += n & 1;
-            n >>>= 1;
+            n >>>= 1;   // >>> logic change high bit to 0, >> arithmetic change high bit to sign
+        }
+        return cnt;
+    }
+
+    /**
+     * n & (n - 1) will remove the latest 1 of the number
+     */
+    public int hammingWeight_2(int n) {
+        int cnt = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            cnt++;
         }
         return cnt;
     }
